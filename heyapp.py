39cd22ec962aae9798cbeb950da58962a868b7f6
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2024-05-05 09:21:40 -0600

    Add files via upload

--- heyapp.py
+++ heyapp.py
@@ -3,6 +3,8 @@ import pandas as pd
 import json
 import numpy as np
 import pandas as pd
+from datetime import datetime
+
 
 def procesar_fecha(fecha):
     # Esta función puede ser personalizada para procesar la fecha como quieras
@@ -14,7 +16,34 @@ st.title('Hey Radar')
 # Selector de fecha en la interfaz
 fecha_usuario = st.date_input("Elige una fecha:")
 
+
+with open("grouped_data.json", "r") as f:
+  data = json.load(f)
+
+def get_week_number(date_string):
+    # Parse the date string into a datetime object
+    target_date = datetime.strptime(date_string, "%Y-%m-%d")
+    
+    # Calculate the week number using the isocalendar() method
+    week_number = target_date.isocalendar()[1]
+    
+    return week_number
+
+def get_data_by_year_and_week(json_array, entrada):
+    entrada = entrada.strftime('%Y-%m-%d')
+    year = entrada[:4]
+    week = get_week_number(entrada)
+
+    year = int(year)
+    week = int(week)
+
+    for entry in json_array:
+        if entry["year"] == year and entry["week"] == week:
+            return entry
+    return None
+
+
 # Botón para procesar la fecha
 if st.button('Realizar analisis'):
-    resultado = procesar_fecha(fecha_usuario)
+    resultado = get_data_by_year_and_week(data, fecha_usuario)
     st.write(resultado)  # Mostrar el resultado procesado
